a47b90b9349eb63de6228c51897bdbc93d7b6bbe
DongHyeok Lim <zx07050@naver.com>
2023-01-23 10:17:32 +0900

    Style: Padding modify

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -7,7 +7,7 @@ const Blog = () => {
   return (
     <div>
       <Header />
-      <div className="w-4/5 mx-auto mt-8 md:px-5 md:py-5 md:flex-row items-center">
+      <div className="w-4/5 mx-auto mt-8 md:px-3 md:py-3 md:flex-row items-center">
         <div className="flex mt-10 mb-10 text-black body-font bg-transparent relative p-10 rounded-[14px] shadow-default hover:-translate-y-1 hover:scale-100 duration-300">
           <div className="mt-4 block">
             <h1 className="block font-bold text-3xl mb-5 font-nanumBold">
