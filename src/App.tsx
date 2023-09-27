79fb05345f9c4fe397d4c3590df9267c23dd8b76
DongHyeok Lim <jh07050@gmail.com>
2023-09-27 17:08:12 +0900

    fix: 새로고침 에러 수정 작업 #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import React from "react"
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom"
-import RealMain from "./pages/Main/RealMain"
-import About from "./pages/About/About"
-import Teams from "./pages/Teams/Teams"
-import TeamsOechul from "./pages/Teams/TeamsOechul"
-import TeamsGbus from "./pages/Teams/TeamsGbus"
-import TeamsHufsSpetition from "./pages/Teams/TeamsHufsSpetition"
-import Recruit from "./pages/Recruit/Recruit"
-import Activity from "./pages/Activity/Activity"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RealMain from "./pages/Main/RealMain";
+import About from "./pages/About/About";
+import Teams from "./pages/Teams/Teams";
+import TeamsOechul from "./pages/Teams/TeamsOechul";
+import TeamsGbus from "./pages/Teams/TeamsGbus";
+import TeamsHufsSpetition from "./pages/Teams/TeamsHufsSpetition";
+import Recruit from "./pages/Recruit/Recruit";
+import Activity from "./pages/Activity/Activity";
 
 function App() {
   return (
-    <BrowserRouter basename="/hufstory">
-      <Router>
-        <Routes>
-          <Route path="/" element={<RealMain />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/activity" element={<Activity />} />
-          <Route path="/Teams" element={<Teams />} />
-          <Route path="/TeamsOechul" element={<TeamsOechul />} />
-          <Route path="/TeamsGbus" element={<TeamsGbus />} />
-          <Route path="/TeamsHufsSpetition" element={<TeamsHufsSpetition />} />
-          <Route path="/recruit" element={<Recruit />} />
-        </Routes>
-      </Router>
-    </BrowserRouter>
+    <Router basename="/hufstroy">
+      <Routes>
+        <Route path="/" element={<RealMain />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/activity" element={<Activity />} />
+        <Route path="/Teams" element={<Teams />} />
+        <Route path="/TeamsOechul" element={<TeamsOechul />} />
+        <Route path="/TeamsGbus" element={<TeamsGbus />} />
+        <Route path="/TeamsHufsSpetition" element={<TeamsHufsSpetition />} />
+        <Route path="/recruit" element={<Recruit />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
